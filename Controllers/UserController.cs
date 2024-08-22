921cd7d2fb7bb611154bb6cb704ee18650d48ee1
kakha <kakha.gujejiani.1@iliauni.edu.ge>
2024-08-22 19:26:56 +0400

    added put adn post routes

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace DotnetAPI.Controllers
-{
+namespace DotnetAPI.Controllers;
+
     [ApiController]
     [Route("[controller]")]
     public class UserController : ControllerBase
@@ -51,7 +51,52 @@ namespace DotnetAPI.Controllers
 
             return response;
         }
+        [HttpPut("EditUser")]
+        public IActionResult EditUser(User user) {
+            string sql = @"
+                UPDATE TutorialAppSchema.Users 
+                SET [FirstName]='" + user.FirstName +
+                "', [LastName]='" + user.LastName +
+                "', [Email]='" + user.Email +
+                "', [Gender]='" + user.Gender +
+                "', [Active]= '" + user.Active +
+                 "' WHERE UserId = " + user.UserId;
+
+            Console.WriteLine(sql);
+            if(_dapper.executeSql(sql)){
+                return Ok();
+            }
+            
+             throw new Exception("Failed to update user");
+        }
+
+        [HttpPost("AddUser")]
+        public IActionResult AddUser(User user) {
+            string sql = @"
+               INSERT INTO TutorialAppSchema.Users(
+                    [FirstName],
+                    [LastName],
+                    [Email],
+                    [Gender],
+                    [Active]
+                    ) VALUES (" +
+                "'" + user.FirstName +
+                "', '" + user.LastName +
+                "', '" + user.Email +
+                "', '" + user.Gender +
+                "', '" + user.Active +
+                "')";
+
+                Console.WriteLine(sql);
+
+            if(_dapper.executeSql(sql)){
+                return Ok();
+            }
+            
+             throw new Exception("Failed to create user");
+        }
+
+
     }
 
     
-}
